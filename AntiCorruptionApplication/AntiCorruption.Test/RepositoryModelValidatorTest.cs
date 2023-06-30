c4d0791f5cc771ee595dfdc62ec0003d07d8d60c
Gustavo Almeida <gustavoalmeida40@hotmail.com>
2023-06-30 07:47:00 -0300

    RepositoryModelValidator Test Added.

--- AntiCorruptionApplication/AntiCorruption.Test/RepositoryModelValidatorTest.cs
+++ AntiCorruptionApplication/AntiCorruption.Test/RepositoryModelValidatorTest.cs
@@ -31,5 +31,108 @@ namespace AntiCorruption.Test
             Assert.IsTrue(result.Errors.Count == 0);
             #endregion
         }
+
+        [TestMethod]
+        public void RepositoryModelValidator_InvalidLicense_Empty_NOK()
+        {
+            #region Builder
+            var builder = new RepositoryModelBuilder();
+            builder.WithId(10);
+            builder.WithAutoInit(true);
+            builder.WithPrivate(true);
+            builder.WithDescription("desc");
+            builder.WithLicenseTemplate("");
+            builder.WithName("name");
+
+            var repositoryModel = builder.Build();
+            #endregion
+
+            #region Validator
+            var validator = new RepositoryModelValidator();
+            var result = validator.Validate(repositoryModel);
+            #endregion
+
+            #region Asserts
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count != 0);
+            #endregion
+        }
+
+        [TestMethod]
+        public void RepositoryModelValidator_InvalidName_Empty_NOK()
+        {
+            #region Builder
+            var builder = new RepositoryModelBuilder();
+            builder.WithId(10);
+            builder.WithAutoInit(true);
+            builder.WithPrivate(true);
+            builder.WithDescription("desc");
+            builder.WithLicenseTemplate("license");
+            builder.WithName("");
+
+            var repositoryModel = builder.Build();
+            #endregion
+
+            #region Validator
+            var validator = new RepositoryModelValidator();
+            var result = validator.Validate(repositoryModel);
+            #endregion
+
+            #region Asserts
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count != 0);
+            #endregion
+        }
+        [TestMethod]
+        public void RepositoryModelValidator_InvalidLicense_Null_NOK()
+        {
+            #region Builder
+            var builder = new RepositoryModelBuilder();
+            builder.WithId(10);
+            builder.WithAutoInit(true);
+            builder.WithPrivate(true);
+            builder.WithDescription("desc");
+            builder.WithLicenseTemplate(null);
+            builder.WithName("name");
+
+            var repositoryModel = builder.Build();
+            #endregion
+
+            #region Validator
+            var validator = new RepositoryModelValidator();
+            var result = validator.Validate(repositoryModel);
+            #endregion
+
+            #region Asserts
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count != 0);
+            #endregion
+        }
+
+        [TestMethod]
+        public void RepositoryModelValidator_InvalidName_Null_NOK()
+        {
+            #region Builder
+            var builder = new RepositoryModelBuilder();
+            builder.WithId(10);
+            builder.WithAutoInit(true);
+            builder.WithPrivate(true);
+            builder.WithDescription("desc");
+            builder.WithLicenseTemplate("license");
+            builder.WithName(null);
+
+            var repositoryModel = builder.Build();
+            #endregion
+
+            #region Validator
+            var validator = new RepositoryModelValidator();
+            var result = validator.Validate(repositoryModel);
+            #endregion
+
+            #region Asserts
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count != 0);
+            #endregion
+        }
     }
 }
